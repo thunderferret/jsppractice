8a1fefb46bfd00f14197dd64e6194cbba00b188c
SGT SEO <35893826+thunderferret@users.noreply.github.com>
2020-02-19 23:55:25 +0900

    update

--- JAVA_PRAC/IOStream/src/SystemIn.java
+++ JAVA_PRAC/IOStream/src/SystemIn.java
@@ -1,13 +1,29 @@
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
 public class SystemIn {
-    public static void main(String []){
+    public static void main(String [] args){
 
 
 
-        System.out.println("slkdfjslkjflks");
+        System.out.println("Stydysdlfj");
 
-        //Byte 단위로 읽어 들인다.
-        
-        System.in.read();
+        //Read bytes
+
+        try{
+            int i;
+            InputStreamReader isr= new InputStreamReader(System.in);// sub Stream let reader
+            while ((i = isr.read())!='x') {
+                System.out.println(i);
+                System.out.println((char) i);
+
+            }
+        }catch (IOException e){
+
+            e.printStackTrace();
+        }
+
 
     }
 
