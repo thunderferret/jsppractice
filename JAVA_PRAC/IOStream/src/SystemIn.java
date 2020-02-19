b7dc84f62bfe94f389c7c408d7380552fb18e185
SGT SEO <35893826+thunderferret@users.noreply.github.com>
2020-02-19 23:44:55 +0900

    JAVA IO STREAM STUDY

---
+++ JAVA_PRAC/IOStream/src/SystemIn.java
@@ -0,0 +1,14 @@
+public class SystemIn {
+    public static void main(String []){
+
+
+
+        System.out.println("slkdfjslkjflks");
+
+        //Byte 단위로 읽어 들인다.
+        
+        System.in.read();
+
+    }
+
+}
